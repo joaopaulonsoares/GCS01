9a9b988a858d2fb826142a86009909f81d04d7c6
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-08-23 19:07:12 -0300

    Changing settings

--- djangobasico/settings.py
+++ djangobasico/settings.py
@@ -117,8 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
