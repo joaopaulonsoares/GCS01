bb64702e518e54e85d5ae2abfeb5bc2b88ea607b
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-08-23 19:03:08 -0300

    Changing static dir settings

--- djangobasico/settings.py
+++ djangobasico/settings.py
@@ -120,3 +120,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
